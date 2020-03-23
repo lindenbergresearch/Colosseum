d6a5570e871706f7a63b3afb3659614f5f55d7fb
patricklindenberg <patrick@heapdump.com>
2020-03-23 17:28:16 +0100

    add serializer and dump methods

--- Util.cs
+++ Util.cs
@@ -1,4 +1,46 @@
+using System;
+using System.IO;
 using Godot;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using File = Godot.File;
+using Object = System.Object;
+
+/// <summary>
+/// Base class for serializing data classes
+/// </summary>
+public abstract class SerializableDataClass {
+	public abstract int VERSION_MAJOR { get; }
+	public abstract int VERSION_MINOR { get; }
+	public abstract int VERSION_PATCH { get; }
+
+	/// <summary>
+	/// Timestamp of serialization
+	/// </summary>
+	public DateTimeOffset Timestamp { get; set; }
+
+	/// <summary>
+	/// Type of serialized class
+	/// </summary>
+	public string Type { get; set; }
+
+
+	/// <summary>
+	/// Returns the version as string
+	/// </summary>
+	/// <returns></returns>
+	public string GetVersionStr() => $"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}";
+
+
+	/// <summary>
+	/// Standard Constructor
+	/// </summary>
+	protected SerializableDataClass() {
+		Timestamp = DateTime.Now;
+		Type = GetType().FullName;
+	}
+}
+
 
 /// <summary>
 ///     Utility method compilation.
@@ -50,5 +92,78 @@ public static class Util {
 	/// <returns></returns>
 	public static Color Color(float r, float g, float b, float a = 1.0f) {
 		return new Color(r, g, b, a);
+	}
+
+
+	/// <summary>
+	/// Serializes an object to a json file.
+	/// </summary>
+	/// <param name="clazz">The class instance to serialize</param>
+	/// <param name="filename">The target filename (including .json)</param>
+	public static void SerializeObject<T>(T clazz, string filename) where T : SerializableDataClass {
+		var options = new JsonSerializerOptions {
+			WriteIndented = true,
+			MaxDepth = 200
+		};
+
+		var jsonString = JsonSerializer.Serialize<T>(clazz, options);
+		System.IO.File.WriteAllText(filename, jsonString);
+	}
+
+
+	/// <summary>
+	/// Deserialize an object based on a json file.
+	/// </summary>
+	/// <param name="filename">The filename of the json file</param>
+	/// <typeparam name="T">The target type to be deserialized</typeparam>
+	/// <returns>And instance of the deserialized file</returns>
+	public static T DeserializeObject<T>(string filename) where T : SerializableDataClass {
+		var jsonString = System.IO.File.ReadAllText(filename);
+		return JsonSerializer.Deserialize<T>(jsonString);
+	}
+
+
+	/// <summary>
+	/// Dump all fields from an object and it's references.
+	/// For debugging issues.
+	/// </summary>
+	/// <param name="clazz">The object reference to dump</param>
+	/// <param name="prefix">Prefix to the output</param>
+	/// <param name="sopen">The formatting open character</param>
+	/// <param name="sclose">THe formatting close character</param>
+	/// <returns></returns>
+	public static string Dump(System.Object clazz, string prefix = "", string sopen = "[", string sclose = "]") {
+		var info = $"{prefix}{sopen}type=";
+		var t = clazz.GetType();
+
+		info += t.Name;
+
+		foreach (var fieldInfo in t.GetFields()) {
+			var name = fieldInfo.Name;
+			var type = fieldInfo.FieldType.Name;
+			var value = fieldInfo.GetValue(null);
+
+			info += $"{name}={type}({value}) ";
+		}
+
+		foreach (var propertyInfo in t.GetProperties()) {
+			var name = propertyInfo.Name;
+			var type = propertyInfo.PropertyType;
+			dynamic value = null;
+			try {
+				value = propertyInfo.GetValue(clazz);
+			}
+			catch (Exception e) {
+				value = "?";
+			}
+
+			if (!type.IsPrimitive && !type.IsValueType && type != typeof(String) && type != typeof(Decimal)) {
+				info += name + "=" + Dump(value, "*" + prefix) + " ";
+			}
+
+			info += $"{name}={type.Name}({value}) ";
+		}
+
+		return info.TrimEnd() + sclose;
 	}
 }
