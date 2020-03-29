bc74ec2ec294c167f12c7c9829b8aadfc55c8540
patricklindenberg <patrick@heapdump.com>
2020-03-29 16:35:09 +0200

    initial commit

---
+++ Player2D.cs
@@ -0,0 +1,28 @@
+using Godot;
+
+/// <summary>
+/// 
+/// </summary>
+public class Player2D : KinematicBody2D {
+
+	public override void _Draw() {
+		base._Draw();
+	}
+
+
+	public override void _PhysicsProcess(float delta) {
+		base._PhysicsProcess(delta);
+	}
+
+
+	public override void _Process(float delta) {
+		base._Process(delta);
+	}
+
+
+	public override void _Ready() {
+		base._Ready();
+	}
+
+
+}
