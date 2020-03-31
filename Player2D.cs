05a66f0d3e73f295ae2a1c897cca8e94a25c6701
patricklindenberg <patrick@heapdump.com>
2020-03-31 17:30:24 +0200

    add routing methods and key functions

--- Player2D.cs
+++ Player2D.cs
@@ -1,28 +1,114 @@
 using Godot;
+using static Godot.Input;
 
-/// <summary>
-/// 
-/// </summary>
-public class Player2D : KinematicBody2D {
+namespace Colosseum {
 
-	public override void _Draw() {
-		base._Draw();
+	/// <summary>
+	/// 
+	/// </summary>
+	public abstract class Player2D : KinematicBody2D {
+
+		/// <summary>
+		/// Motion vector
+		/// </summary>
+		public static Motion2D motion = new Vector2(0, 0);
+
+
+		/// <summary>
+		/// Map action keys
+		/// </summary>
+		protected static class ActionKey {
+
+			public static bool Up {
+				get => IsActionPressed("ui_up");
+			}
+
+			public static bool Down {
+				get => IsActionPressed("ui_down");
+			}
+
+			public static bool Left {
+				get => IsActionPressed("ui_left");
+			}
+
+			public static bool Right {
+				get => IsActionPressed("ui_right");
+			}
+
+			public static bool Run {
+				get => IsActionPressed("ui_accept");
+			}
+
+			public static bool Jump {
+				get => IsActionPressed("ui_cancel");
+			}
+
+			public static bool Select {
+				get => IsActionPressed("ui_select");
+			}
+
+		}
+
+
+		/// <summary>
+		/// Routing function for Draw event.
+		/// </summary>
+		public abstract void Draw();
+
+
+		/// <summary>
+		/// Routing function for PhysicsProcess event.
+		/// </summary>
+		/// <param name="delta">Delta time in seconds</param>
+		public abstract void PhysicsProcess(float delta);
+
+
+		/// <summary>
+		/// Routing function for Process event.
+		/// </summary>
+		/// <param name="delta">Delta time in seconds</param>
+		public abstract void Process(float delta);
+
+
+		/// <summary>
+		/// Routing function Ready event.
+		/// </summary>
+		public abstract void Ready();
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public override void _Draw() {
+			Draw();
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="delta"></param>
+		public override void _PhysicsProcess(float delta) {
+			PhysicsProcess(delta);
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="delta"></param>
+		public override void _Process(float delta) {
+			Process(delta);
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public override void _Ready() {
+			Ready();
+		}
+
 	}
-
-
-	public override void _PhysicsProcess(float delta) {
-		base._PhysicsProcess(delta);
-	}
-
-
-	public override void _Process(float delta) {
-		base._Process(delta);
-	}
-
-
-	public override void _Ready() {
-		base._Ready();
-	}
-
 
 }
