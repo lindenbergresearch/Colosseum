a4de59fe7f0e65cb20479c5dfadd9968e052409a
patricklindenberg <patrick@heapdump.com>
2020-04-02 21:40:58 +0200

    update test

--- Main.cs
+++ Main.cs
@@ -4,32 +4,36 @@ using static System.Console;
 using static State;
 
 
-public class MainApp {
-	[NativeState]
-	public bool Test { get; set; } = true;
-
-	[NativeState]
-	public bool Foo { get; set; } = true;
-
-	[NativeState]
-	public bool Bar { get; set; }
+public class Foo {
 
 
-	[NativeState]
-	public bool IsGrounded() => true;
+	public bool IsOnFloor() {
+		return true;
+	}
 	
 	
+
+}
+
+public class MainApp : Foo {
+
+
+	[NativeState(bind:"IsOnFloor")]
+	 State Grounded = null;
+
+	
+	public MainApp() {
+		this.SetupNativeStates();
+	}
+
+
 	static void Main(string[] args) {
-		var p = new MainApp();
-		p.SetupNativeStates();
-
-		Func<bool> f = p.IsGrounded;
 		
-		PolyState foo = (PolyState)p.IsGrounded + p.IsGrounded + p.IsGrounded;
-
-
-		foreach (var nativeState in NativeStates) {
-			WriteLine($"State: {nativeState.Key} = {nativeState.Value.Resolve()}");
-		}
+		var ma = new MainApp();
+		
+		
+		
+		WriteLine($"result: {ma.Grounded}");
+		
 	}
 }
