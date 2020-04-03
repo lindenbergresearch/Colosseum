87b632959b639b331a6fb57b5a2a5989cd3776ef
lindenbergresearch <lindenberg.research@gmail.com>
2020-04-03 12:37:13 +0200

    update

---
+++ RMath.cs
@@ -0,0 +1,15 @@
+﻿namespace Renoir {
+    /// <summary>
+    /// Extension methods for numeric stuff 
+    /// </summary>
+    public static class RMath {
+        /// <summary>
+        /// Test for number exceeding a special value
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool exceeds(this float x, float y)
+            => x > y;
+    }
+}
