b4d16c76d2587292663dce4d2577c4eae9f63174
patricklindenberg <patrick@heapdump.com>
2020-04-13 10:56:36 +0200

    add consuming and collision

--- AgileItem2D.cs
+++ AgileItem2D.cs
@@ -5,28 +5,28 @@ using Renoir;
 
 
 /// <summary>
-/// 
+/// Basic consumable item
 /// </summary>
-public class AgileItem2D : KinematicBody2D, ICollidable {
-	private static Vector2 GRAVITY = new Vector2(0, 900);
-	private static Vector2 INITIAL_IMPULSE = new Vector2(50, -250);
+public class AgileItem2D : KinematicBody2D, ICollider {
+	private static readonly Vector2 GRAVITY = new Vector2(0, 900);
+	private static readonly Vector2 INITIAL_IMPULSE = new Vector2(50, -250);
 
 	private Motion2D Motion { get; set; } = (0, 0);
+	private Motion2D Save { get; set; } = (0, 0);
 
-	public bool Active { get; set; } = false;
-
+	public bool Active { get; set; }
 
 	[GNode("Label")] private Label _label;
-
-
-	
-
 
 	/// <summary>
 	/// 
 	/// </summary>
+	/// <param name="sender"></param>
 	/// <param name="collision"></param>
-	public void onCollide(KinematicCollision2D collision) {
+	public void OnCollide(object sender, KinematicCollision2D collision) {
+		Logger.debug($"coll: {sender.GetType().FullName}");
+		if (sender is IConsumer consumer)
+			consumer.OnConsume(this);
 	}
 
 
@@ -36,18 +36,23 @@ public class AgileItem2D : KinematicBody2D, ICollidable {
 	/// <param name="delta"></param>
 	public override void _PhysicsProcess(float delta) {
 		if (!Active) return;
-
-
 		if (!Visible) Visible = true;
 
 		foreach (var collision2D in this.GetCollider()) {
-			if (collision2D.Normal != Motion2D.FLOOR_NORMAL) {
-				Logger.debug($"position={collision2D.Position} velocity={collision2D.ColliderVelocity} collider={collision2D.Collider} vector={collision2D.Normal}");
-				Motion *= collision2D.Normal;
+			if (collision2D.Collider is IConsumer consumer) {
+				consumer.OnConsume(this);
+				continue;
 			}
+
+			if (collision2D.Top()) continue;
+
+			Logger.debug($"position={collision2D.Position} motion={Save} velocity={collision2D.ColliderVelocity} collider={collision2D.Collider} vector={collision2D.Normal}");
+			Motion = Save * -collision2D.Normal.Abs();
 		}
 
-		_label.Text = $"Motion= {Motion}";
+		Save = Motion.Velocity;
+
+		_label.Text = $"Motion={Motion}";
 
 		Motion += delta * GRAVITY;
 		Motion = MoveAndSlide(Motion, Motion2D.FLOOR_NORMAL);
@@ -57,8 +62,9 @@ public class AgileItem2D : KinematicBody2D, ICollidable {
 	/// <summary>
 	/// 
 	/// </summary>
-	public void Activate() {
-		_label.Visible = true;
+	public void Activate(QuestionBox.ContentType type) {
+		_label.Visible = Logger.Level == Logger.LogLevel.TRACE;
+
 		Active = true;
 		Motion += INITIAL_IMPULSE;
 	}
