4563850a04bbb4678d815f99a12bb0f61af9e8b8
patricklindenberg <patrick@heapdump.com>
2020-04-17 15:24:26 +0200

    update to new property system

--- PlayerInfoText.cs
+++ PlayerInfoText.cs
@@ -3,41 +3,29 @@ using Renoir;
 using static Renoir.PropertyPool;
 
 
-public class PlayerInfoText : ParallaxBackground, IPropertyChangeListener {
+public class PlayerInfoText : ParallaxBackground, IPropertyChangeHandler {
 	[GNode("CoinsLabel")] private Label CoinsLabel;
 	[GNode("LevelNameLabel")] private Label LevelNameLabel;
 	[GNode("LivesLabel")] private Label LivesLabel;
 	[GNode("ScoreLabel")] private Label ScoreLabel;
 	[GNode("TimeLabel")] private Label TimeLabel;
-
-	[Register("main.level.name", "$main.playerinfo")] 
-	public Property<string> pLevelName { get; set; }
-
-	[Register("main.level.time", "$main.playerinfo", "{0:D3}")]
-	public Property<int> pTime { get; set; }
-	[Register("main.player.coins", "$main.playerinfo", "{0:D3}")]
-	public Property<int> pCoins { get; set; }
-
-	[Register("main.player.lives", "$main.playerinfo", "{0:D2}")]
-	public Property<int> pLives { get; set; }
-
-	[Register("main.player.score", "$main.playerinfo", "{0:D7}")]
-	public Property<int> pScore { get; set; }
+	[GNode("FPSLabel")] private Label FPSLabel;
 
 
 	public void OnPropertyChange<T>(Property<T> sender, PropertyEventArgs<T> args) {
-		ScoreLabel.Text = pScore;
-		TimeLabel.Text = pTime;
-		CoinsLabel.Text = pCoins;
-		LivesLabel.Text = pLives;
-		LevelNameLabel.Text = pLevelName;
+		FPSLabel.Text = Node2D.FPS;
+		ScoreLabel.Text = Mario2D.TotalScore;
+		TimeLabel.Text = Node2D.LevelTime;
+		CoinsLabel.Text = Mario2D.CollectedCoins;
+		LivesLabel.Text = Mario2D.LivesLeft;
+		LevelNameLabel.Text = Node2D.LevelName;
 	}
 
 
 	public override void _Ready() {
 		this.SetupGlobalProperties();
 		this.SetupNodeBindings();
-
-		AddSubscription(this, "$main.playerinfo");
+		
+		AddSubscription("main.*", this);
 	}
 }
