98b4e771acd7555c7ea697b9579b1d2caae0f391
lindenbergresearch <lindenberg.research@gmail.com>
2020-04-22 11:51:43 +0200

    test logger

--- Main.cs
+++ Main.cs
@@ -4,18 +4,13 @@ using System.Linq;
 using Renoir;
 using static System.Console;
 
-
-
 public class MainApp {
 
-	private static bool Something => true;
-	
-	
-	
 	
 	
 	private static void Main(string[] args) {
 
+		Logger.debug("Hello world from Main!");
 
 		
 	}
