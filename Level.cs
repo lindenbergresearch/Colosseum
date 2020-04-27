718393c34694b9ddd5f641f5bd79315ae1fd6bd1
patricklindenberg <patrick@heapdump.com>
2020-04-27 17:33:26 +0200

    add docs and type hint

--- Level.cs
+++ Level.cs
@@ -1,23 +1,36 @@
+using System;
 using Godot;
+using static Renoir.PropertyExtensions;
 
 namespace Renoir {
-
 
 	/// <summary>
 	/// Property class:
 	/// Holds all properties for the current level
 	/// </summary>
 	public static class Level {
+		/// <summary>
+		/// </summary>
+		public static Type Type { get; } = typeof(Level);
 
+		/// <summary>
+		/// The full name of the level
+		/// </summary>
 		[Register("Main.Level.Name")]
 		public static string Name { get; set; }
 
+		/// <summary>
+		/// The levels gravity vector
+		/// </summary>
 		[Register("Main.Level.Gravity")]
 		public static Vector2 Gravity { get; set; }
 
 
+		/// <summary>
+		/// Static Init / setup global properties via ca's
+		/// </summary>
 		static Level() {
-			PropertyExtensions.SetupGlobalProperties(typeof(Level));
+			SetupGlobalProperties(typeof(Level));
 		}
 
 	}
