88e470d5370b5bdaaea4a6c97623c841ffec31b7
patricklindenberg <patrick@heapdump.com>
2020-04-28 06:20:44 +0200

    update

--- Main.cs
+++ Main.cs
@@ -1,11 +1,24 @@
+using System;
+using Newtonsoft.Json.Linq;
 using Renoir;
 
 
 public class MainApp {
 
+	public static int field1 = 1;
+	public static string field2 = "hello";
+	public static float field3 = 2.3124f;
+	private static int k = 1;
+
 
 	private static void Main(string[] args) {
-		Logger.debug("Hello world from Main!");
+		var d = Util.ListProperties(typeof(Level));
+
+
+		foreach (var pair in d) {
+			Console.WriteLine($"Key: {pair.Key} Value: {pair.Value}");
+		}
+		
 	}
 
 }
