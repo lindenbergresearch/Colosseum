3a91ef336bdf1766ec092c322178013ba7c338cd
Patrick Lindenberg <heapdump@icloud.com>
2022-04-05 10:29:41 +0200

    reformat

--- scripts/ActionKey.cs
+++ scripts/ActionKey.cs
@@ -23,17 +23,19 @@ using Godot;
 
 #endregion
 
-namespace Renoir;
+namespace Renoir {
 
-/// <summary>
-///     Helper class for key shortcuts
-/// </summary>
-public static class ActionKey {
-	public static bool Up => Input.IsActionPressed("ui_up");
-	public static bool Down => Input.IsActionPressed("ui_down");
-	public static bool Left => Input.IsActionPressed("ui_left");
-	public static bool Right => Input.IsActionPressed("ui_right");
-	public static bool Run => Input.IsActionPressed("ui_accept");
-	public static bool Jump => Input.IsActionJustPressed("ui_cancel");
-	public static bool Select => Input.IsActionJustPressed("ui_select");
+	/// <summary>
+	///     Helper class for key shortcuts
+	/// </summary>
+	public static class ActionKey {
+		public static bool Up => Input.IsActionPressed("ui_up");
+		public static bool Down => Input.IsActionPressed("ui_down");
+		public static bool Left => Input.IsActionPressed("ui_left");
+		public static bool Right => Input.IsActionPressed("ui_right");
+		public static bool Run => Input.IsActionPressed("ui_accept");
+		public static bool Jump => Input.IsActionJustPressed("ui_cancel");
+		public static bool Select => Input.IsActionJustPressed("ui_select");
+	}
+
 }
