8784f7f2bd58418e3d5fef73fa33aa9a470bc24b
Patrick Lindenberg <heapdump@icloud.com>
2022-04-05 10:31:42 +0200

    add bitmap font and godot tool

---
+++ bmpfont/BitmapFont.cs
@@ -0,0 +1,347 @@
+#region header
+
+// 
+//    _____
+//   (, /   )            ,
+//     /__ /  _ __   ___   __
+//  ) /   \__(/_/ (_(_)_(_/ (_  CORE LIBRARY
+// (_/ ______________________________________/
+// 
+// 
+// Renoir Core Library for the Godot Game-Engine.
+// Copyright 2020-2022 by Lindenberg Research.
+// 
+// www.lindenberg-research.com
+// www.godotengine.org
+// 
+
+#endregion
+
+#region
+
+using System;
+using Godot;
+using Renoir;
+using static Renoir.Util;
+using static Renoir.Logger;
+
+#endregion
+
+
+/// <summary>
+/// </summary>
+internal class BitmapFontConfig : SerializableDataClass {
+	public override int VERSION_MAJOR { get; } = 1;
+	public override int VERSION_MINOR { get; } = 0;
+	public override int VERSION_PATCH { get; } = 0;
+
+	public Vector<int> GlyphDimension { get; set; } = new(0, 0);
+	public float LineHeight { get; set; } = 2.0f;
+	public float Scale { get; set; } = 1.0f;
+	public bool HasShadow { get; set; } = false;
+	public int Offset { get; set; } = 0;
+	public Vector<float> ShadowOffset { get; set; } = new(2, 2);
+
+
+	/// <summary>
+	///     POCO for holding 2D vector data
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class Vector<T> {
+		public Vector() { }
+
+
+		public Vector(T x, T y) {
+			X = x;
+			Y = y;
+		}
+
+
+		public T X { get; set; }
+		public T Y { get; set; }
+	}
+}
+
+
+/// <summary>
+/// </summary>
+public class BitmapGlyph {
+	/// <summary>
+	/// </summary>
+	private readonly int[,] data;
+
+	private readonly int height;
+	private readonly int width;
+
+
+	/// <summary>
+	///     Construct a new bitmap glyph with a given dimension.
+	/// </summary>
+	/// <param name="width"></param>
+	/// <param name="height"></param>
+	public BitmapGlyph(int width, int height) {
+		this.width = width;
+		this.height = height;
+
+		data = new int[width, height];
+	}
+
+
+	/// <summary>
+	///     Parses the image data and converts it to a 1-bit data array.
+	/// </summary>
+	/// <param name="image"></param>
+	/// <param name="offset"></param>
+	/// <param name="transparent"></param>
+	public void Parse(Image image, Vector2 offset, Color transparent) {
+		image.Lock();
+
+		for (var j = 0; j < height; j++)
+			for (var i = 0; i < width; i++) {
+				var pixel = image.GetPixel((int) offset.x + i, (int) offset.y + j);
+
+				if (pixel == transparent) data[i, j] = 0;
+				else data[i, j] = 1;
+			}
+
+		image.Unlock();
+	}
+
+
+	/// <summary>
+	///     Tests for pixel at the given coordinate
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <returns></returns>
+	public bool PixelAt(int x, int y) {
+		return data[x, y] == 1;
+	}
+}
+
+
+/// <summary>
+///     Simple class to load and use plain bitmap files as bitmap font in Godot.
+/// </summary>
+public class BitmapFont {
+	public static readonly int MIN_CHAR_DIMENSION = 4;
+
+
+	private BitmapGlyph[] _glyphs;
+
+
+	/// <summary>
+	///     Set default values on creation
+	/// </summary>
+	public BitmapFont() {
+		Offset = 32;
+		Count = 256;
+
+		CharsDimension = Vec(1, 1);
+		GlyphDimension = Vec(8, 16);
+		TransparentColor = Color(0, 0, 0, 0);
+	}
+
+
+	/// <summary>
+	///     The color which indicates the transparent background.
+	/// </summary>
+	public Color TransparentColor { get; set; }
+
+	/// <summary>
+	///     The the characters count vertical and horizontal.
+	///     Will be autodetect.
+	/// </summary>
+	public Vector2 CharsDimension { get; set; }
+
+	/// <summary>
+	///     The dimension of one character in pixels.
+	/// </summary>
+	public Vector2 GlyphDimension { get; set; }
+
+	/// <summary>
+	///     The offset of the character index.
+	/// </summary>
+	public int Offset { get; set; }
+
+	/// <summary>
+	///     The character count.
+	/// </summary>
+	public int Count { get; set; }
+
+
+	/// <summary>
+	///     The source image
+	/// </summary>
+	public Texture imageTexture { get; set; }
+
+	/// <summary>
+	///     Holds all parsed BitmapGlyph instances.
+	/// </summary>
+
+
+	public bool IsLoaded { get; set; }
+
+
+	/// <summary>
+	/// </summary>
+	/// <param name="index"></param>
+	/// <returns></returns>
+	public BitmapGlyph GetGlyph(int index) {
+		if (_glyphs == null || _glyphs.Length <= index) return null;
+
+		return _glyphs[index];
+	}
+
+
+	public int GetGlyphCount() {
+		if (_glyphs != null) return _glyphs.Length;
+		return -1;
+	}
+
+
+	/// <summary>
+	///     Detects the correct char dimension based on the glyph dimension
+	/// </summary>
+	public void DetectCharsDimension() {
+		trace($"{this}");
+
+		if (imageTexture == null) return;
+
+		if ((int) GlyphDimension.x < MIN_CHAR_DIMENSION || (int) GlyphDimension.y < MIN_CHAR_DIMENSION) return;
+
+		var idim = new Vector2(imageTexture.GetData().GetWidth(), imageTexture.GetData().GetHeight());
+
+		if (idim.x < MIN_CHAR_DIMENSION || idim.y < MIN_CHAR_DIMENSION) return;
+
+		if ((int) idim.x % (int) GlyphDimension.x != 0) {
+			GD.PrintErr(
+				$"No possible character x-alignment found: texture-with={idim.x}px char-width:{GlyphDimension.x}px ratio:{idim.x / GlyphDimension.x} !?");
+
+			return;
+		}
+
+		if ((int) idim.y % (int) GlyphDimension.y != 0) {
+			GD.PrintErr(
+				$"No possible character y-alignment found: texture-height={idim.y}px char-height:{GlyphDimension.y}px ratio:{idim.y / GlyphDimension.y} !?");
+
+			return;
+		}
+
+		// throw new BitmapFontException($"The height of the image: {idim.y}px doesn't fit to the glyphs height: {GlyphDimension.y}px!");
+		// throw new BitmapFontException($"The width of the image: {idim.x}px doesn't fit to the glyphs width: {GlyphDimension.x}px!");
+
+		CharsDimension = new Vector2(idim.x / GlyphDimension.x, idim.y / GlyphDimension.y);
+		GD.Print($"Character alignment is: {CharsDimension}");
+	}
+
+
+	/// <summary>
+	///     Detect transparent color while retrieving the color of a specific point.
+	///     The default is (0, 0)
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <returns></returns>
+	private Color DetectTransparentColor(int x = 0, int y = 0) {
+		trace($"{this}");
+
+		var image = imageTexture.GetData();
+
+		image.Lock();
+		var color = image.GetPixel(x, y);
+		image.Unlock();
+
+		return color;
+	}
+
+
+	/// <summary>
+	///     Try to process the given image with the given configuration.
+	/// </summary>
+	/// <returns>True if the image could be processed to a bitmap-font</returns>
+	private bool ProcessImage() {
+		trace($"{this}");
+
+		var image = imageTexture.GetData();
+		var cindex = 0;
+
+		if ((int) CharsDimension.x < MIN_CHAR_DIMENSION || (int) CharsDimension.y < MIN_CHAR_DIMENSION) return false;
+
+		if (CharsDimension.x * CharsDimension.y > 512) {
+			GD.PrintErr($"Suspicious character alignment: {CharsDimension.x} x {CharsDimension.y} !?");
+			return false;
+		}
+
+		var transparentColor = DetectTransparentColor();
+
+		_glyphs = new BitmapGlyph[(int) CharsDimension.x * (int) CharsDimension.y];
+
+		for (var y = 0; y < CharsDimension.y; y++)
+			for (var x = 0; x < CharsDimension.x; x++) {
+				var glyph = new BitmapGlyph((int) GlyphDimension.x, (int) GlyphDimension.y);
+				glyph.Parse(image, Vec(x * GlyphDimension.x, y * GlyphDimension.y), transparentColor);
+
+				if (cindex < _glyphs.Length)
+					_glyphs[cindex] = glyph;
+				else GD.PrintErr($"Impossible: {CharsDimension} index: {cindex}");
+
+				cindex++;
+			}
+
+		return true;
+	}
+
+
+	/// <summary>
+	/// </summary>
+	public void process() {
+		trace($"{this}");
+		IsLoaded = false;
+		CharsDimension = Vec(0, 0);
+
+		if (imageTexture?.GetData() == null) return;
+
+		GD.Print(
+			$"{GetType().Name}: Loading bitmap-font with: {this} from image: {imageTexture.ResourceName} having {imageTexture.GetData().GetSize()} px.");
+
+		DetectCharsDimension();
+
+		if (!ProcessImage()) {
+			GD.PrintErr($"{GetType().Name}: Error loading bitmap-font with current config: {Dump(this)}!");
+			return;
+		}
+
+
+		GD.Print($"{GetType().Name}: Create bitmap-font: {this}");
+		IsLoaded = true;
+	}
+
+
+	/// <summary>
+	/// </summary>
+	/// <returns></returns>
+	public override string ToString() {
+		return
+			$"{GetType().Name}: " +
+			$"GlyphDimension={GlyphDimension} " +
+			$"CharsDimension={CharsDimension} " +
+			$"Offset={Offset} " +
+			$"Count={Count} " +
+			$"Texture={imageTexture?.ResourceName}";
+	}
+}
+
+
+/// <summary>
+///     Bitpmap Font Exception
+/// </summary>
+public class BitmapFontException : Exception {
+	public BitmapFontException() { }
+
+
+	public BitmapFontException(string message) : base(message) { }
+
+
+	public BitmapFontException(string message, Exception innerException) : base(message, innerException) { }
+}
---
+++ bmpfont/BitmapFont2D.cs
@@ -0,0 +1,384 @@
+#region header
+
+// 
+//    _____
+//   (, /   )            ,
+//     /__ /  _ __   ___   __
+//  ) /   \__(/_/ (_(_)_(_/ (_  CORE LIBRARY
+// (_/ ______________________________________/
+// 
+// 
+// Renoir Core Library for the Godot Game-Engine.
+// Copyright 2020-2022 by Lindenberg Research.
+// 
+// www.lindenberg-research.com
+// www.godotengine.org
+// 
+
+#endregion
+
+#region
+
+using System;
+using Godot;
+using static Renoir.Util;
+using static Renoir.Logger;
+using File = System.IO.File;
+
+#endregion
+
+
+/// <summary>
+///     Tool node to edit and preview a bitmap font in realtime.
+/// </summary>
+[Tool]
+public class BitmapFont2D : Godot.Node2D {
+	private Color _foreground;
+	private Vector2 _glyphDimension;
+	private bool _hasShadow;
+	private Texture _imageTexture;
+	private float _lineHeight;
+	private int _offset;
+	private Vector2 _scale;
+	private Color _shadow;
+	private Vector2 _shadowOffset;
+	private string _text;
+
+	private bool lockRefresh;
+
+
+	/// <summary>
+	///     Setup standard values
+	/// </summary>
+	public BitmapFont2D() {
+		_glyphDimension = Vec(8, 16);
+		_foreground = Color(1, 1, 1);
+		_text = "The quick brown fox is dead. !\"§$\"%&/()=?><|#+*'´`/\\";
+		_scale = Vec(1, 1);
+		_lineHeight = 2;
+		_hasShadow = false;
+		_shadow = Color(0, 0, 0);
+		_shadowOffset = Vec(1, 1);
+
+		_offset = 32;
+
+		BitmapFont = new BitmapFont();
+	}
+
+
+	private BitmapFont BitmapFont { get; }
+
+
+	[Export]
+	public bool SaveToJson {
+		get => false;
+		set => SaveConfigToJSON();
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public Texture ImageTexture {
+		get => _imageTexture;
+		set {
+			_imageTexture = value;
+
+			if (_imageTexture != null && _imageTexture.ResourcePath.Length > 3) {
+				// test of there is a file equal to the image file ending with json
+				var jsonFile = _imageTexture.ResourcePath.Substring(6) + ".json";
+				if (File.Exists(jsonFile)) {
+					GD.Print($"Found a corresponding json config file for this bitmap-font: {jsonFile}. Try to load it...");
+					ConfigureFromJSON(jsonFile);
+				}
+			}
+
+			ConfigureBitmapFont();
+			Refresh();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public Vector2 GlyphDimension {
+		get => _glyphDimension;
+		set {
+			var v = value;
+
+			if ((int) v.x < 1) v.x = 1;
+			if ((int) v.y < 1) v.y = 1;
+
+			_glyphDimension = v;
+
+			ConfigureBitmapFont();
+			Refresh();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public int Offset {
+		get => _offset;
+		set {
+			_offset = value;
+			ConfigureBitmapFont();
+			Refresh();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public Color Foreground {
+		get => _foreground;
+		set {
+			_foreground = value;
+			Refresh();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export(PropertyHint.MultilineText)]
+	public string Text {
+		get => _text;
+		set {
+			_text = value;
+			Refresh();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public Vector2 TextScale {
+		get => _scale;
+		set {
+			_scale = value;
+			Refresh();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public Vector2 CharsDimension { get; set; }
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public float LineHeight {
+		get => _lineHeight;
+		set {
+			_lineHeight = value;
+			Refresh();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public Color Shadow {
+		get => _shadow;
+		set {
+			_shadow = value;
+			Update();
+		}
+	}
+
+
+	/// <summary>
+	/// </summary>
+	[Export]
+	public bool HasShadow {
+		get => _hasShadow;
+		set {
+			_hasShadow = value;
+			Update();
+		}
+	}
+
+
+	[Export]
+	public Vector2 ShadowOffset {
+		get => _shadowOffset;
+		set {
+			_shadowOffset = value;
+			Update();
+		}
+	}
+
+
+	/// <summary>
+	///     Transfer local properties to BitmapFont
+	/// </summary>
+	private void ConfigureBitmapFont() {
+		BitmapFont.imageTexture = _imageTexture;
+		BitmapFont.Offset = _offset;
+		BitmapFont.CharsDimension = CharsDimension;
+		BitmapFont.GlyphDimension = _glyphDimension;
+	}
+
+
+	/// <summary>
+	///     Simulates a put pixel taking scale into account.
+	/// </summary>
+	/// <param name="pos">The position (in normal coordinates)</param>
+	/// <param name="color">The color</param>
+	private void PutPixel(Vector2 pos, Color color) {
+		DrawRect(new Rect2(pos * _scale, _scale), color);
+	}
+
+
+	/// <summary>
+	/// </summary>
+	/// <param name="pos"></param>
+	/// <param name="text"></param>
+	private void DrawText(Vector2 pos, string text, Color color) {
+		for (var c = 0; c < text.Length; c++) {
+			var index = Mathf.Clamp(text[c] - _offset, 0, BitmapFont.GetGlyphCount() - 1);
+			var glyph = BitmapFont.GetGlyph(index);
+
+
+			for (var y = 0; y < GlyphDimension.y; y++)
+				for (var x = 0; x < GlyphDimension.x; x++)
+					if (glyph.PixelAt(x, y))
+						PutPixel(Vec(x + c * GlyphDimension.x, y) + pos, color);
+		}
+	}
+
+
+	/// <summary>
+	///     Draw bitmap-font to canvas.
+	/// </summary>
+	public override void _Draw() {
+		if (_imageTexture == null || _imageTexture.GetData() == null || BitmapFont == null || BitmapFont.GetGlyphCount() == 0 ||
+		    !BitmapFont.IsLoaded)
+			return;
+
+
+		var lines = Text.Split('\n');
+		var yoffset = 0;
+
+		foreach (var line in lines) {
+			if (HasShadow) DrawText(Vec(0, yoffset * (GlyphDimension.y + _lineHeight)) + ShadowOffset, line, Shadow);
+
+			DrawText(Vec(0, yoffset * (GlyphDimension.y + _lineHeight)), line, Foreground);
+			yoffset++;
+		}
+	}
+
+
+	/// <summary>
+	///     Take properties and try to reload the bitmap-font.
+	/// </summary>
+	private void Refresh() {
+		trace($"Refresh(): {this}");
+
+		if (lockRefresh) return;
+
+		try {
+			if (_imageTexture?.GetData() == null) {
+				trace($"Refresh(): no texture data.");
+				return;
+			}
+
+			if (!BitmapFont.IsLoaded) {
+				trace($"Refresh(): not loaded!");
+				BitmapFont.process();
+			}
+			//SaveConfigToJSON();
+
+
+			if (BitmapFont.IsLoaded) {
+				CharsDimension = BitmapFont.CharsDimension;
+				Update();
+			} else {
+				GD.PrintErr($"{GetType().Name}: Unable to use bitmap-font with that configuration: {Dump(BitmapFont)}");
+			}
+		} catch (Exception e) {
+			GD.PrintErr(e);
+			trace("Exception: " + e);
+		}
+
+		trace($"After Refresh(): {this}");
+	}
+
+
+	/// <summary>
+	/// </summary>
+	public void SaveConfigToJSON() {
+		if (_imageTexture == null || _imageTexture.GetData() == null || _imageTexture.ResourcePath.Length < 3) return;
+		var jsonFile = _imageTexture.ResourcePath.Substring(6) + ".json";
+
+
+		try {
+			var _config = new BitmapFontConfig {
+				GlyphDimension = {
+					X = (int) GlyphDimension.x,
+					Y = (int) GlyphDimension.y
+				},
+				Scale = Scale.x,
+				Offset = Offset,
+				HasShadow = HasShadow,
+				ShadowOffset = {
+					X = (int) ShadowOffset.x,
+					Y = (int) ShadowOffset.y
+				}
+			};
+
+			SerializeObject(_config, jsonFile);
+		} catch (Exception e) {
+			GD.PrintErr($"Unable to save bitmap-file config data to json file: '{jsonFile}'");
+			GD.PrintErr($"Exception is: {e.Message}");
+		}
+	}
+
+
+	/// <summary>
+	///     Setup bitmap properties from json data.
+	/// </summary>
+	/// <param name="filename"></param>
+	public void ConfigureFromJSON(string filename) {
+		lockRefresh = true;
+
+		try {
+			var _config = DeserializeObject<BitmapFontConfig>(filename);
+
+			GlyphDimension = new Vector2(_config.GlyphDimension.X, _config.GlyphDimension.Y);
+			Scale = new Vector2(_config.Scale, _config.Scale);
+			Offset = _config.Offset;
+			LineHeight = _config.LineHeight;
+			HasShadow = _config.HasShadow;
+			ShadowOffset = new Vector2(_config.ShadowOffset.X, _config.ShadowOffset.Y);
+
+			GD.Print("Bitmap-font successfully configured by json file.");
+		} catch (Exception e) {
+			GD.PrintErr($"Unable to load bitmap-font config data from json file: {filename}");
+			GD.PrintErr(e.Message);
+		} finally {
+			lockRefresh = false;
+		}
+	}
+
+
+	public override string ToString() {
+		return
+			$"GlyphDimension={GlyphDimension} Scale={Scale} Offset={Offset} LineHeight={LineHeight} HasShadow={HasShadow} ShadowOffset={ShadowOffset} CharDimensions={CharsDimension}";
+	}
+
+
+	/// <summary>
+	/// </summary>
+	public override void _Ready() { }
+}
